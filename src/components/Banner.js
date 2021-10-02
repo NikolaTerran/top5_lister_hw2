572f564a9db4b53331a00bb39a1d61b916ee37e4
nikolaterran <nikolaterranthe1@gmail.com>
2021-10-02 17:28:42 -0400

    1. can load list now

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,7 +3,7 @@ import EditToolbar from "./EditToolbar";
 
 export default class Banner extends React.Component {
     render() {
-        const { title} = this.props;
+        const {title} = this.props;
         return (
             <div id="top5-banner">
                 {title}
--- src/components/Workspace.js
+++ src/components/Workspace.js
@@ -2,6 +2,8 @@ import React from "react";
 
 export default class Workspace extends React.Component {
     render() {
+        const {currentList} = this.props;
+        const isNull = (currentList === null)
         return (
             <div id="top5-workspace">
                 <div id="workspace-edit">
@@ -12,6 +14,23 @@ export default class Workspace extends React.Component {
                         <div className="item-number">4.</div>
                         <div className="item-number">5.</div>
                     </div>
+                    {isNull ?
+                        <div id="edit-items">
+                        <div id="item-1" className="top5-item"></div>
+                        <div id="item-1" className="top5-item"></div>
+                        <div id="item-1" className="top5-item"></div>
+                        <div id="item-1" className="top5-item"></div>
+                        <div id="item-1" className="top5-item"></div>
+                        </div>
+                        :
+                        <div id="edit-items">
+                            <div id="item-1" className="top5-item">{currentList.items[0]}</div>
+                            <div id="item-1" className="top5-item">{currentList.items[1]}</div>
+                            <div id="item-1" className="top5-item">{currentList.items[2]}</div>
+                            <div id="item-1" className="top5-item">{currentList.items[3]}</div>
+                            <div id="item-1" className="top5-item">{currentList.items[4]}</div>
+                        </div>
+                    }
                 </div>
             </div>
         )
